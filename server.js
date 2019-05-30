d0246e9734c5e0ea65f2269e406bef262a31f53e
quincyshadow <inversenormal@gmail.com>
2019-05-30 10:26:47 -0700

    prod

--- server.js
+++ server.js
@@ -6,6 +6,105 @@ const port = process.env.PORT || 8000;
 let storage = [];
 app.use(bodyParser.json());
 
+//
+app.post('/users', function(req, res) {
+  var user = req.body;
+
+  if (!user) {
+    return res.sendStatus(400);
+  }
+  storage.push(user);
+  res.json(storage);
+});
+
+app.get('/users', function(req, res) {
+  var user = req.body;
+
+  if (!user) {
+    return res.sendStatus(400);
+  }
+
+  res.json(storage);
+});
+
+// Get route for getting a user by name
+
+app.get('/users/:name', function(req, res) {
+  // var user = req.body;
+  var name = req.params.name;
+
+  console.log(name);
+
+  if (!name) {
+    return res.sendStatus(400);
+  }
+
+  let foundUs = false;
+
+  //breaks on first oc
+  storage.map( (e,i) =>
+  {
+    if (e['name'] == name)
+    {
+      res.json(e);
+    }
+  })
+
+});
+
+// Update route for updating a user by name
+app.put('/users/:name', function(req, res) {
+  var user = req.body;
+  var name = req.params.name;
+
+  console.log(name);
+
+  if (!name) {
+    return res.sendStatus(400);
+  }
+
+  let foundUs = false;
+
+  //breaks on first oc
+  storage.map( (e,i) =>
+  {
+    if (e['name'] == name)
+    {
+      storage[i] = user;
+      res.json(storage[i]);
+    }
+  })
+
+});
+
+// Delete route for deleting a user by name
+
+app.delete('/users/:name', function(req, res) {
+  var user = req.body;
+  var name = req.params.name;
+
+  console.log(name);
+
+  if (!name) {
+    return res.sendStatus(400);
+  }
+
+  let foundUs = false;
+
+  //breaks on first oc
+  storage.map( (e,i) =>
+  {
+    if (e['name'] == name)
+    {
+      ret = storage.splice(i,1)[0];
+      res.json(ret);
+    }
+  })
+
+});
+
+
+
 
 app.listen(port, ()=>{
   console.log(`Listening on port ${port}`);
